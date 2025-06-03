1900d9dd6ba1b70b5578bcf027666faaac7a770d
TrueJole <jole.koop@gmail.com>
2025-06-03 17:16:43 +0200

    Added useful floor image

--- Scripts/grid.gd
+++ Scripts/grid.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@onready var save: Button = $VBoxContainer/Save
+@onready var copy: Button = $VBoxContainer/Copy
 
 func _on_save_pressed() -> void:
 	print('Saving')
@@ -9,6 +11,46 @@ func _on_save_pressed() -> void:
 		JavaScriptBridge.download_buffer(buf,"Grid_" + Time.get_date_string_from_system() + "_" + Time.get_time_string_from_system().replace(":", "-") + ".jpg")
 	else:
 		img.save_jpg(OS.get_system_dir(OS.SYSTEM_DIR_PICTURES) + "/Grid_" + Time.get_date_string_from_system() + "_" + Time.get_time_string_from_system().replace(":", "-") + ".jpg", 0.75)
-	$Save.text = "Saved"
+	save.text = "Saved"
 	await get_tree().create_timer(3).timeout
-	$Save.text = "Save Image"
+	save.text = "Save Image"
+
+var copyText: Array[Array] = [
+	["Error", 3],
+	["Error", 3],
+	["stop", 3],
+	["Error", 3],
+	["it's not working", 3],
+	["Error", 3],
+	["take a hint, dude", 3],
+	["Error", 3],
+	["""LOOK I have spent HOURS trying to get 
+	this copy feature to work. But I have failed.
+	Instead, I decided to put in this little easter egg.
+	How ironic. Next time your annoyed that you have
+	to drag and drop the downloaded image into the
+	Discord, think about this instead.
+	I apologize.""", 30],
+	["Copied Succesfully", 3]
+	]
+
+var counter: int
+func _on_copy_pressed() -> void:
+	print('Copying')
+	var img: Image = $SubViewportContainer/SubViewport.get_texture().get_image()
+	if OS.get_name() == "Web":
+		copy.text = copyText[counter][0]
+		if counter == 9:
+			var navigator = JavaScriptBridge.get_interface("navigator")
+			var clipboard = navigator.clipboard
+			clipboard.writeText("Gotcha")
+	else:
+		print('Feature not available')
+		img.save_jpg(OS.get_system_dir(OS.SYSTEM_DIR_PICTURES) + "/Grid_" + Time.get_date_string_from_system() + "_" + Time.get_time_string_from_system().replace(":", "-") + ".jpg", 0.75)
+		copy.text = "NA"
+		
+	await get_tree().create_timer(copyText[counter][1]).timeout
+	counter += 1;
+	if counter >= 10:
+		counter = 0
+	copy.text = "Copy Image"
--- Scripts/gridX.gd
+++ Scripts/gridX.gd
@@ -5,7 +5,7 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if get_parent().get_parent().get_parent().get_parent().get_parent().sandbags[x_coordinate-1][y_coordinate-1]:
+	if $"../../../../..".get_parent().sandbags[x_coordinate-1][y_coordinate-1]:
 		visible = true
 	else:
 		visible = false
